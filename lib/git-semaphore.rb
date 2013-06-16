43850e7e199b6486f7c225625ffd25c0d6e1ff70
Peter Vandenberk <pvandenberk@mac.com>
2013-06-16 09:47:44 +0100

    Consistently use single quotes

--- lib/git-semaphore.rb
+++ lib/git-semaphore.rb
@@ -3,12 +3,12 @@ require 'rubygems'
 require 'grit'
 require 'trollop'
 
-require "git-semaphore/version"
-require "git-semaphore/banner"
-require "git-semaphore/copyright"
+require 'git-semaphore/version'
+require 'git-semaphore/banner'
+require 'git-semaphore/copyright'
 
-require "git-semaphore/app"
-require "git-semaphore/api"
+require 'git-semaphore/app'
+require 'git-semaphore/api'
 
 module Git
   module Semaphore
