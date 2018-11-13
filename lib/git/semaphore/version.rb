cde8c5f97d3fd42b588ef7f0f7ddfbe4fbfdf1af
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2018-11-13 08:51:11 +0000

    make it work when deps not installed yet

--- lib/git/semaphore/version.rb
+++ lib/git/semaphore/version.rb
@@ -1,15 +1,6 @@
-require 'rugged'
-
 module Git
   module Semaphore
     NAME = 'git-semaphore'.freeze
     VERSION = '2.3.1'.freeze
-
-    DEPENDENCY_VERSIONS = [
-      "rugged v#{Rugged::VERSION}",
-      "#{RUBY_ENGINE} #{RUBY_VERSION}p#{RUBY_PATCHLEVEL}",
-    ].join(', ').freeze
-
-    LONG_VERSION = "#{NAME} v#{VERSION} (#{DEPENDENCY_VERSIONS})".freeze
   end
 end
